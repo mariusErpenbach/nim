abf8b6807e2efca517be6b93659520f0acfd2f29
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2021-11-18 22:37:15 +0100

    leaderboard

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -11,7 +11,13 @@ const boardStats = () => {
    
   console.log(props.leaderList)
     const items = props.leaderList.map((item, i) => ( 
-       <div> {i}</div>
+      <div>
+      <div>{item.Name}</div>
+      <div>{item.Sticks}</div>
+      <div>{item.Turns}</div>
+      <div>{item.Time}s</div>
+      <div> score : {item.Sticks - item.Turns}</div>
+      </div>
       ));      
 
     return (
