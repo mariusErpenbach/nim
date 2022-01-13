4bdbbed36b474ecb008f69f9fdae7bb31b568eb8
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2022-01-13 03:56:48 +0100

    update

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,53 +1,60 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import StackSplitter from "./StackSplitter";
 import PlayerTurn from "./PlayerTurn";
 import ComputerTurn from "./ComputerTurn";
-const Game = (props) =>{
+const Game = (props) => {
+  const [stackOne, setstackOne] = useState(0);
+  const [stackTwo, setstackTwo] = useState(0);
+  const [stackThree, setstackThree] = useState(0);
+  const [NoS, setNoS] = useState(0);
+  const [Turn, setTurn] = useState("");
 
-const [stackOne, setstackOne] = useState(0)
-const [stackTwo, setstackTwo] = useState(0)
-const [stackThree, setstackThree] = useState(0)
-const [NoS, setNoS] = useState(0)
-const [Turn, setTurn] = useState("")
-
-const stackSplitterFinished = () => {
-setstackOne(document.getElementById("stackOne").innerHTML)
-setstackTwo(document.getElementById("stackTwo").innerHTML)
-setstackThree(document.getElementById("stackThree").innerHTML)
-let parentElement = document.getElementById("game")
-let stackSplitterElement = document.getElementsByClassName("stackSplitter")
-parentElement.removeChild(stackSplitterElement[0])
-}
-    return (
-        <React.Fragment>
-        <div id="game">
-        <header id="exitFrame">  
-        <Link to="/"><i className="fas fa-times"></i></Link>
+  const stackSplitterFinished = () => {
+    setstackOne(document.getElementById("stackOne").innerHTML);
+    setstackTwo(document.getElementById("stackTwo").innerHTML);
+    setstackThree(document.getElementById("stackThree").innerHTML);
+    let parentElement = document.getElementById("game");
+    let stackSplitterElement = document.getElementsByClassName("stackSplitter");
+    parentElement.removeChild(stackSplitterElement[0]);
+  };
+  return (
+    <React.Fragment>
+      <div id="game">
+        <header id="exitFrame">
+          <Link to="/">
+            <i className="fas fa-times"></i>
+          </Link>
         </header>
         <div id="stackArea">
-        <main>
-        <div className="stacks"><p id="stackOne"></p></div>
-        <div className="stacks"><p id="stackTwo"></p></div>
-        <div className="stacks"><p id="stackThree"></p></div>
-        </main>
-        <aside><p id="bigStack"></p></aside>
+          <main>
+            <div className="stacks">
+              <p id="stackOne"></p>
+            </div>
+            <div className="stacks">
+              <p id="stackTwo"></p>
+            </div>
+            <div className="stacks">
+              <p id="stackThree"></p>
+            </div>
+          </main>
+          <aside>
+            <p id="bigStack"></p>
+          </aside>
         </div>
-        
-        {Turn==="Player"? <PlayerTurn/>:<div/>}
-        {Turn==="Computer"? <ComputerTurn/>:<div/>}
-        <StackSplitter  finisher={stackSplitterFinished}
-        active={true}
-        stickAmount={NoS}
-        stackOne = {stackOne}
-        stackTwo = {stackTwo}
-        stackThree = {stackThree}
-         />
-        
- 
-        </div>
-        </React.Fragment>
-    )
-}
 
-export default Game
+        {Turn === "Player" ? <PlayerTurn /> : <div />}
+        {Turn === "Computer" ? <ComputerTurn /> : <div />}
+        <StackSplitter
+          finisher={stackSplitterFinished}
+          stickAmount={NoS}
+          stackOne={stackOne}
+          stackTwo={stackTwo}
+          stackThree={stackThree}
+        />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Game;
--- src/components/StackSplitter.jsx
+++ src/components/StackSplitter.jsx
@@ -1,75 +1,78 @@
-import react,{useState,useEffect} from "react"
+import react, { useState, useEffect } from "react";
 
 const StackSplitter = (props) => {
+  const [numberOfSticks, setnumberOfSticks] = useState(props.stickAmount);
+  const [visible, setvisible] = useState(true);
 
-    const [numberOfSticks, setnumberOfSticks] = useState(props.stickAmount)
-    const [visible, setvisible] = useState(true)
-    
+  const splitOne = (button) => {
+    let stack = document.getElementById("stackOne");
+    let bigStack = document.getElementById("bigStack");
+    // subtract 40% from the bigStack to the first stack (first computer move)
+    let randomNumber = Math.floor(Math.random() * (75 - 5) + 5);
+    if(randomNumber <1){randomNumber =1}
+    stack.innerHTML = Math.floor((numberOfSticks / 100) * randomNumber);
+    setnumberOfSticks(
+      numberOfSticks - Math.floor((numberOfSticks / 100) * randomNumber)
+    );
+    bigStack.innerHTML =
+      numberOfSticks - Math.floor((numberOfSticks / 100) * randomNumber);
+    button.innerHTML = "sticks to staple two";
+  };
 
-    const splitOne = (button) =>{
-        let stack = document.getElementById("stackOne")
-        let bigStack = document.getElementById("bigStack")
-        // subtract 40% from the bigStack to the first stack (first computer move)
-        let randomNumber = Math.floor(Math.random()*(75-1)+1);
-        console.log(randomNumber)
-        stack.innerHTML = Math.floor((numberOfSticks/100)*randomNumber)
-        setnumberOfSticks(numberOfSticks-Math.floor((numberOfSticks/100)*randomNumber))
-        bigStack.innerHTML = numberOfSticks-Math.floor((numberOfSticks/100)*randomNumber)
-        button.innerHTML = "sticks to staple two"
-        
+  const splitTwo = (userValue) => {
+    let stackTwo = document.getElementById("stackTwo");
+    let stackThree = document.getElementById("stackThree");
+    let bigStack = document.getElementById("bigStack");
+    // let user split the bigStack one more time (but 1 stick needs to be left in the bigstack for the third stack)
+    stackTwo.innerHTML = userValue;
+    bigStack.innerHTML -= userValue;
+    // split the remaining sticks from bigStack to stackThree
+    stackThree.innerHTML = bigStack.innerHTML;
+    bigStack.innerHTML = "";
+    // update states
+    setnumberOfSticks(numberOfSticks - userValue);
+  
+    props.finisher();
+  };
+
+  const lockStack = () => {
+    let stackOne = document.getElementById("stackOne");
+    let stackTwo = document.getElementById("stackTwo");
+    let inputButton = document.getElementById("inputButton");
+    var inputfield = document.getElementById("stickAmount");
+    let bigStack = document.getElementById("bigStack");
+
+    if (stackOne.innerHTML < 1  ) {
+    if (inputfield.value <=2){return alert ("send atleast 3 sticks to the big staple")}
+      // if stackOne is filled already -> skip splitOne
+      // clear input
+      inputfield.value = "";
+      return splitOne(inputButton);
+    } else if (stackTwo.innerHTML < 1) {
+      // check if input was low enough to split once more after the user split
+      if (parseInt(bigStack.innerHTML) <= numberOfSticks) {
+        return alert("to much input");
+      } else {
+        inputfield.value = "";
+        return splitTwo(numberOfSticks);
+      }
     }
-    const splitTwo = (userValue) =>{
-        let stackTwo = document.getElementById("stackTwo")
-        let stackThree = document.getElementById("stackThree")
-        let bigStack = document.getElementById("bigStack")
-        // let user split the bigStack one more time (but 1 stick needs to be left in the bigstack for the third stack)
-        stackTwo.innerHTML = userValue
-        bigStack.innerHTML -= userValue;
-        // split the remaining sticks from bigStack to stackThree
-        stackThree.innerHTML = bigStack.innerHTML;
-        bigStack.innerHTML = "";
-        // update states
-        setnumberOfSticks(numberOfSticks-userValue)
-        // setvisible(0)
-        props.finisher()
-    }
-    
-    
-    
-    const lockStack = () =>{
-        let stackOne = document.getElementById("stackOne")
-        let stackTwo = document.getElementById("stackTwo")
-        let inputButton = document.getElementById("inputButton")
-        var inputfield = document.getElementById("stickAmount")
-        let bigStack = document.getElementById("bigStack")
-    
-        if (stackOne.innerHTML<1){ // if stackOne is filled already -> skip splitOne
-        // clear input
-            inputfield.value= ""
-            return splitOne(inputButton)
-        }
-        else if (stackTwo.innerHTML<1){ 
-            // check if input was low enough to be splittet 2 more times
-            if (parseInt(bigStack.innerHTML)<=numberOfSticks){
-                return  alert("to much input")
-            }     
-            else{
-            inputfield.value= ""
-            return splitTwo(numberOfSticks)}
-        }
-    }
+  };
 
+  return (
+    <div className="stackSplitter">
+      <input
+        id="stickAmount"
+        type="number"
+        onChange={() => {
+          setnumberOfSticks(document.getElementById("stickAmount").value);
+        }}
+      ></input>
+      <button id="inputButton" onClick={lockStack}>
+        lock stack
+      </button>
+    </div>
+  );
+};
 
-return props.active === true ?  (
-<div className="stackSplitter">
-
-
-<input id="stickAmount" type="number" onChange={()=>{setnumberOfSticks(document.getElementById("stickAmount").value)}}></input> 
-<button id="inputButton" onClick={lockStack}>lock stack</button>
- 
-        
-</div>) : <div/>
-
-}
-
-export default StackSplitter
+export default StackSplitter;
